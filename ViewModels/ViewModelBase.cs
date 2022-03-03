29a357ba5bec4955df52840a2d35448d17aaa98c
Meeperty <meepprograms@gmail.com>
2022-03-02 17:42:30 -0800

    Avalonia project added

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOCD_Sharp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
