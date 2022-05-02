cdb02a861f1f3e5d6ffbea76dff3b8dd5cbaf4ea
Meeperty <meepprograms@gmail.com>
2022-05-01 17:26:04 -0700

    C dll for windows

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace SOCD_Sharp.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
+    //public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
+    //{
+    //    public event PropertyChangedEventHandler? PropertyChanged;
+
+    //    string buttonText = "Hi";
+
+    //    public string ButtonText
+    //    {
+    //        get { return buttonText; }
+    //        set
+    //        {
+    //            buttonText = value;
+    //            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(buttonText)));
+    //        }
+    //    }
+
+    //    public void ButtonClicked() => ButtonText = "... ";
+    //}
 }
