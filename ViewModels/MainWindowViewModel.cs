9940087215063841c808d6722e2b41f91972c9e4
Meeperty <meepprograms@gmail.com>
2022-05-02 19:00:54 -0700

    Messed up, back to blank Avalonia project

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,26 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using System.ComponentModel;
 
 namespace SOCD_Sharp.ViewModels
 {
-    //public class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
-    //{
-    //    public event PropertyChangedEventHandler? PropertyChanged;
-
-    //    string buttonText = "Hi";
-
-    //    public string ButtonText
-    //    {
-    //        get { return buttonText; }
-    //        set
-    //        {
-    //            buttonText = value;
-    //            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(buttonText)));
-    //        }
-    //    }
-
-    //    public void ButtonClicked() => ButtonText = "... ";
-    //}
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,5 +1,3 @@
-using System;
-using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -20,59 +18,5 @@ namespace SOCD_Sharp.Views
         {
             AvaloniaXamlLoader.Load(this);
         }
-    }
-
-    public class MainWindowViewModel : INotifyPropertyChanged
-    {
-        //string buttonText = "Click Me!";
-
-        //public string ButtonText
-        //{
-        //    get => buttonText;
-        //    set 
-        //    {
-        //        buttonText = value;
-        //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonText)));
-        //    }
-        //}
-
-        //public void ButtonClicked() => ButtonText = "Hello, Avalonia!";
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        #region Radio Button properties
-        public bool wasdSet = false;
-        public bool WASDSet
-        {
-            get => wasdSet;
-            set
-            {
-                wasdSet = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(WASDSet)));
-            }
-        }
-
-        public bool arrowsSet = false;
-        public bool ArrowsSet
-        {
-            get => arrowsSet;
-            set
-            {
-                arrowsSet = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ArrowsSet)));
-            }
-        }
-
-        public bool customSet = false;
-        public bool CustomSet
-        {
-            get => customSet;
-            set
-            {
-                customSet = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CustomSet)));
-            }
-        }
-        #endregion
     }
 }
