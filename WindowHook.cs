870d3d264c2a988d217c472b903ec449a278ec31
Meeperty <meepprograms@gmail.com>
2022-05-08 16:24:55 -0700

    Revert "Revert "added winevent EVENT enum""
    This reverts commit 8726436ac172c831f0f03c460bc5cf15f93660da.

---
+++ WindowHook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOCD_Sharp
+{
+    class WindowHook
+    {
+        private Win32Interop.HookProc windowProc;
+
+        public IntPtr Hook
+        {
+            get;
+            private set;
+        }
+
+        public WindowHook()
+        {
+            //windowProc
+        }
+
+        public void InstallHook()
+        {
+            if (Hook == IntPtr.Zero)
+            {
+                //Hook = Win32Interop.SetWinEventHook(Win32Interop.EVENT.);
+            }
+        }
+    }
+}
