702766e0429c7d7bc6a9686aab3e0e517778c914
Meeperty <meepprograms@gmail.com>
2022-12-11 21:01:20 -0800

    rebinding options for left, up, right, and down, still messing with UI

---
+++ KeyBindDataBindings.cs
@@ -0,0 +1,58 @@
+﻿using socd.ViewModels;
+
+namespace socd
+{
+    public class KeyBindDataBindings : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Update()
+        {
+            PropertyChanged?.Invoke(this, new(nameof(CodeString)));
+        }
+
+        private string codeString;
+        public string CodeString
+        {
+            get
+            {
+                return codeString;
+            }
+            set
+            {
+                codeString = value;
+                if (ushort.TryParse(codeString, System.Globalization.NumberStyles.HexNumber, null, out ushort b))
+                {
+                    bind = b;
+                }
+                PropertyChanged?.Invoke(this, new(nameof(CodeString)));
+                PropertyChanged?.Invoke(this, new(nameof(KeyString)));
+            }
+        }
+
+        public string KeyString
+        {
+            get
+            {
+                if (((VKShort)bind).ToString() != bind.ToString())
+                    return $" = {(VKShort)bind}";
+                else
+                    return " = not a key :(";
+            }
+        }
+
+        private ushort bind;
+        public ushort Bind
+        {
+            get
+            {
+                return bind;
+            }
+            set
+            {
+                bind = value;
+                CodeString = bind.ToString("X");
+            }
+        }
+    }
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -13,29 +13,10 @@ namespace socd.Views
             //Thread.Sleep(5000);
             InitializeComponent();
 
-            Opened += (object? o, EventArgs e) =>
-            {
-                #if DEBUG
-                MessageBox(PlatformImpl.Handle.Handle, "Debug", "Box", 0);
-                #endif
-
-                DBTextBox.AddHandler(KeyUpEvent, OnDBTextInput, Avalonia.Interactivity.RoutingStrategies.Tunnel);
-            };
-
             Closed += (object? o, EventArgs e) =>
             {
                 ((MainWindowViewModel)DataContext).Dispose();
             };
         }
-
-        public void OnDBTextInput(object? o, KeyEventArgs e)
-        {
-            ((MainWindowViewModel)DataContext)?.OnDBTextInput(DBTextBox, e);
-        }
-
-        #if DEBUG
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
-        #endif
     }
 }
